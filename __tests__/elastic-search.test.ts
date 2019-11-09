f84a8cb5569aee0184714c14e1c39a17d269d986
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-11-09 19:01:38 +0900

    added aws-sdk

--- __tests__/elastic-search.test.ts
+++ __tests__/elastic-search.test.ts
@@ -1,3 +1,8 @@
+import * as AWS from 'aws-sdk'
+import { Client } from '@elastic/elasticsearch'
+
+const endpoint = 'https://search-geolonia-dev-i4bo3fxcjmx7dvkycmo6lsbscq.us-east-1.es.amazonaws.com'
+
 test('basic', () => {
     expect(true).toBe(true);
 });
