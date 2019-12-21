1f5864e7c2e923b65b709625b04d6cb0704d4483
hatatatehaze <hatatatehaze@gmail.com>
2019-12-21 10:15:29 -0500

    added geojson

--- __tests__/elastic-search.test.ts
+++ __tests__/elastic-search.test.ts
@@ -4,5 +4,5 @@ import { Client } from '@elastic/elasticsearch'
 const endpoint = 'https://search-geolonia-dev-i4bo3fxcjmx7dvkycmo6lsbscq.us-east-1.es.amazonaws.com'
 
 test('basic', () => {
-    expect(true).toBe(true);
+  expect(true).toBe(true);
 });
